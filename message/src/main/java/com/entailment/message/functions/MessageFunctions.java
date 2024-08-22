9928a670913b87cd5a6e1aa325cd48ae7612f18e
DESKTOP-GRRLEL3\Deepak <deepakpareek1@gmail.com>
2024-08-23 00:46:58 +0530

    added function interface

--- message/src/main/java/com/entailment/message/functions/MessageFunctions.java
+++ message/src/main/java/com/entailment/message/functions/MessageFunctions.java
@@ -1,8 +1,33 @@
 package com.entailment.message.functions;
 
+import java.util.function.Function;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.entailment.message.dto.AccountMsgDto;
 
 @Configuration
 public class MessageFunctions {
+	
+	private static final Logger log = LoggerFactory.getLogger(MessageFunctions.class);
+	
+	@Bean
+	public Function<AccountMsgDto, AccountMsgDto> email() {
+		return accountsMsgDto -> {
+			log.info("Sending email with the details:" + accountsMsgDto.toString());
+			return accountsMsgDto;
+		};
+	}
+	
+	@Bean
+	public Function<AccountMsgDto, Long> sms() {
+		return accountsMsgDto -> {
+			log.info("Sending sms with the details:" + accountsMsgDto.toString());
+			return accountsMsgDto.accountNumber();
+		};
+	}
 
 }
